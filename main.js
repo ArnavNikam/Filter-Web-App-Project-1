4e9c8cb62d7e67bd63ffacb16fac9236afea494d
ArnavNikam <75774457+ArnavNikam@users.noreply.github.com>
2022-05-09 16:38:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function setup() {
+    canvas = createCanvas(400,400);
+    canvas.center();
+}
+
+function draw() {
+
+}
+
+function preload() {
+
+}
+
+function take_snapshot() {
+    save( 'photo_booth.png' );
+}
